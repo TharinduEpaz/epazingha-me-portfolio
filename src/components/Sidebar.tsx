596fc3f8596ba1a6069a7e73172451c40d5fdbed
Tharindu Epasingha <epazingha@gmail.com>
2024-12-01 20:51:00 +0530

    added image and profile name

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,14 +106,14 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src="https://media.licdn.com/dms/image/v2/D5603AQH5TxhhVi91og/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1700930985046?e=1738800000&v=beta&t=j89wuzqCQXd4jBwMCdLC4XKT-JaPO6lGGoxVnN8oXPU"
         alt="Avatar"
         height="40"
         width="40"
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-bold text-primary">John Doe</p>
+        <p className="font-bold text-primary">Tharindu Epasingha</p>
         <p className="font-light text-secondary">Developer</p>
       </div>
     </div>
